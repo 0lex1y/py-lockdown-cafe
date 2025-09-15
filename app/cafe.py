26b76451b05c88d7dc95d6e71fcece2a8115b100
0lex1y <kotov.oleksiy@icloud.com>
2025-09-15 23:02:22 +0300

    Solution2

--- app/cafe.py
+++ app/cafe.py
@@ -1,14 +1,14 @@
 import datetime
-from .errors import (NotVaccinatedError,
-                     OutdatedVaccineError,
-                     NotWearingMaskError)
+from app.errors import (NotVaccinatedError,
+                        OutdatedVaccineError,
+                        NotWearingMaskError)
 
 
 class Cafe:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(self, visitor: dict) -> None:
+    def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("Visitor is not vaccinated")
         expiration = visitor["vaccine"].get("expiration_date")
@@ -16,5 +16,4 @@ class Cafe:
             raise OutdatedVaccineError("Visitor's vaccine is outdated")
         if not visitor.get("wearing_a_mask", False):
             raise NotWearingMaskError("Visitor is not wearing a mask")
-        else:
-            print(f"Welcome to {self.name}")
+        return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
-from .cafe import Cafe
-from .errors import NotVaccinatedError, NotWearingMaskError
+from app.cafe import Cafe
+from app.errors import (VaccineError,
+                        NotWearingMaskError)
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> Cafe:
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
     masks_to_buy = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
-            print(f"Friends can go to {cafe.name}")
-        except NotVaccinatedError:
-            print("All friends should be vaccinated")
+        except VaccineError:
+            return "All friends should be vaccinated"
         except NotWearingMaskError:
             masks_to_buy += 1
 
-    print(f"Friends should buy {masks_to_buy} masks")
+    if masks_to_buy > 0:
+        return f"Friends should buy {masks_to_buy} masks"
+    return f"Friends can go to {cafe.name}"
